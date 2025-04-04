f84b2bb655867368393da4078d74577dcd02c249
Sourav <souravbarua789bd@gmail.com>
2025-04-04 19:18:00 +0600

    added search and filter by region functionality

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import '../styles/Styles.css'
 import {fetchAllCountries} from '../services/FetchCountries'
 import CountryCard from '../components/CountryCard'
+import SearchIcon from '../../public/magnifying-glass-solid.svg'
 
 const Homepage = () => {
 
     const [countries, setCountries] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [selectedRegion, setSelectedRegion] = useState("All");
 
     useEffect( ()=>{
         const getCountries = async () =>{
@@ -16,13 +19,45 @@ const Homepage = () => {
         getCountries();
     }, []);
 
+    const filteredCountries = countries.filter((country) => {
+        const matchesSearch = country.name.common.toLowerCase().includes(searchQuery.toLowerCase());
+        const matchesRegion = selectedRegion === "All" || country.region === selectedRegion;
+        return matchesSearch && matchesRegion;
+    })
+
     return (
         <div>
             <div className="homme-page">
                 <div className="container">
+                    <div className="search-part w-100 d-flex flex-wrap justify-content-between">
+                        <div className="col-lg-5 col-md-6 col-12 search-container d-flex">
+                            <img src={SearchIcon} alt="Search icon" className='sarch-icon' />
+                            <input 
+                            type="text"
+                            placeholder='Search for a country...'
+                            className='search-input'
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)} />
+                        </div>
+
+                        <div className="col-lg-3 col-md-4 col-7 select-container">
+                            <select 
+                            name='region'
+                            className='region-section'
+                            value={selectedRegion}
+                            onChange={(e) => setSelectedRegion(e.target.value)}>
+                                <option className='region-option' value="All">All region</option>
+                                <option className='region-option' value="Asia">Asia</option>
+                                <option className='region-option' value="Africa">Africa</option>
+                                <option className='region-option' value="Americas">Americas</option>
+                                <option className='region-option' value="Europe">Europe</option>
+                                <option className='region-option' value="Oceania">Oceania</option>
+                            </select>
+                        </div>
+                    </div>
                     <div className="row">
                         {
-                        countries.map((country)=>(
+                        filteredCountries.map((country)=>(
                             <CountryCard key={country.cca3} country={country}/>
                         ))
                         }
