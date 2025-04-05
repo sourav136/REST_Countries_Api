9e24dd6929b630af776ee5dd6a983bdaed71a26c
Sourav <souravbarua789bd@gmail.com>
2025-04-05 18:39:29 +0600

    fixing path issues and adding _redirects file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CountryDetail from './components/CountryDetail';
-import HomePage from './pages/Homepage';
+import HomePage from './pages/HomePage';
 
 const App = () => {
   return (
